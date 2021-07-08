cce0503ce2e5120a6bbb0d032ad6715820a48049
Elip195 <elip195@gmail.com>
2021-07-08 16:22:09 +0300

    Fix user tweet count only taking into account the year 2020

--- code/hpc_code/get_author_tweet_count.py
+++ code/hpc_code/get_author_tweet_count.py
@@ -7,10 +7,28 @@ database_path = "/gpfs0/rami/users/iliapl/data/databases/53k_individual_hcps_70_
 
 conn = sql.connect(database_path)
 cur = conn.cursor()
-query = "SELECT author, COUNT(*) FROM posts WHERE date > date('2019-12-31') GROUP BY author"
+query = "SELECT author, COUNT(*) FROM posts GROUP BY author"
 results = cur.execute(query).fetchall()
 
 tweet_count_list = [tweet_count for (_, tweet_count) in results]
+
+# how many users with most posts have 10% of the tweets?
+
+total_number_of_posts = 16616970
+
+num_users = 0
+tweet_sum = 0
+
+total_tweet_percentage = 0.1
+
+for c in sorted(tweet_count_list, reverse=True):
+  print(c)
+  tweet_sum += c
+  num_users += 1
+  if tweet_sum >= total_tweet_percentage * total_number_of_posts:
+    break
+
+print("{} users with the most posts account for {}% of all tweets".format(num_users, tweet_sum / total_number_of_posts * 100.0))
 
 count_count_dict = {count : tweet_count_list.count(count) for count in set(tweet_count_list)}
 
